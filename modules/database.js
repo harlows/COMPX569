cd7c9e4e66ad024ece3d202d59d8541207d443f0
Stephen Harlow <stephen@harlow.net.nz>
2026-06-06 17:40:59 +1200

    Fix DB config: use DB_NAME instead of USER_NAME

--- modules/database.js
+++ modules/database.js
@@ -2,6 +2,7 @@ const mariadb = require("mariadb");
 
 const USER_NAME = process.env.USER_NAME;
 const USER_PASS = process.env.USER_PASS;
+const DB_NAME = process.env.DB_NAME;
 
 const database = mariadb.createConnection({
     host: process.env.HOST,
